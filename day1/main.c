0347a197a917c4090b34f130646bef6d009701e6
OkiStuff <probro1802@gmail.com>
2022-12-05 16:51:09 -0500

    day1 part 2

--- day1/main.c
+++ day1/main.c
@@ -5,13 +5,17 @@ int main(void)
 {
     parser parser_data = parse_data("./input.txt");
     int highest = 0;
-    
+    int second = 0;
+    int third = 0;
+        
     for (int i = 0; i < parser_data.size; i++)
     {
-        if (parser_data.data[i] > highest) highest = parser_data.data[i];
+        if (parser_data.data[i] >= highest) highest = parser_data.data[i];
+        else if (parser_data.data[i] >= second) second = parser_data.data[i];
+        else if (parser_data.data[i] >= third) third = parser_data.data[i];
     }
     
-    printf("Highest: %d\n", highest);
+    printf("Highest: %d\nSecond: %d\nThird: %d\nSum: %d\n", highest, second, third, highest + second + third);
     
     free(parser_data.data);
     return 0;
